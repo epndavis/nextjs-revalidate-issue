a04d8fdf0a2178b07d115319eac57fe1b9a26060
epndavis <47059262+epndavis@users.noreply.github.com>
2023-05-14 10:13:50 +0100

    Chore: Update graphqlApi path

--- app/dataGraphQl/page.tsx
+++ app/dataGraphQl/page.tsx
@@ -1,28 +1,4 @@
-import { cache } from 'react'
-
-const getGraphql = cache(async () => {
-  const requestBody = {
-    query: `{ now(id: "2") }`,
-  }
-
-  const res = await fetch('https://main--time-pav6zq.apollographos.net/graphql', {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify(requestBody),
-    next: {
-      tags: ['graphql'],
-      revalidate: 20,
-    },
-  })
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-
-  return res.json();
-})
+import { getGraphql } from '@/lib/graphql'
 
 export default async function Home() {
   const [GRAPHQL, GRAPHQL2] = await Promise.all([getGraphql(), getGraphql()])
